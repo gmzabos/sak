f4855455b6bae15e9223d1c5d08d046a7b94101a
Gerald-Markus Zabos <gmzgames.de@googlemail.com>
2017-07-28 20:47:31 +0200

    upd base_install.sh

--- centos-webserver/base_install.sh
+++ centos-webserver/base_install.sh
@@ -2,21 +2,35 @@
 #
 # gmzabos        28.07.2017
 #
-# Install + Configure webserver
+# Install + Configure Apache webserver
 #
 # Prerequisites:    - CentOS 7 server
 #                   - yum install git
-#
+#                   - git clone https://github.com/gmzabos/sak.git /root/scripts
 
 #########################################
 # Set Variables
 #########################################
-
+SAK="/root/scripts/sak/"
+WEB="/etc/httpd/conf.d/"
 
 #########################################
-#
+# Install epel-release
 #########################################
+yum -y install epel-release
 
+#########################################
+# Install useful tools
+#########################################
+yum -y install wget git bind-utils net-tools telnet libselinux-python unzip tree mailx strace lsof ntp
+
+#########################################
+# Install Apache httpd
+#########################################
+yum -y install httpd
+cp ${SAK}/centos-webserver/www.conf $(WEB)
+service httpd start
+chkconfig httpd on
 
 #########################################
 # Send notification
